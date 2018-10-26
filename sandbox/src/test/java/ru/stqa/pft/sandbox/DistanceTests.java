f6f424e50def6f872c42923776a4c757b9901aad
AnEmelianov <44379615+AnEmelianov@users.noreply.github.com>
2018-10-27 00:52:02 +0300

    homework3 lesson1

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -6,14 +6,16 @@ public class DistanceTests {
 
   @Test
   public void checkDistance() {
-    Point p = new Point(3, 5, 7, 2);
-    Assert.assertEquals(p.area(),5.0);
+    Point p1 = new Point(3,5);
+    Point p2 = new Point(7,2);
+    Assert.assertEquals(p1.distance(p2),5.0);
   }
 
   @Test
   //Данный тест возвращает true/false
   public void checkDistance1() {
-    Point p = new Point(3, 5, 7, 2);
-    System.out.println(p.area() == 5.0);
+    Point p1 = new Point(3,5);
+    Point p2 = new Point(7, 2);
+    System.out.println(p1.distance(p2) == 5.0);
   }
 }
