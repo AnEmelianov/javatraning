3b75b693cc27b3311e8a30332a2ad1e4b5059ef7
AnEmelianov <44379615+AnEmelianov@users.noreply.github.com>
2018-10-28 00:49:12 +0300

    lection lesson2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,17 +5,19 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
 
+
     Square s = new Square(5);
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(5,8);
+    Rectangle r = new Rectangle(5, 8);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
+
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
 
-
 }
+
