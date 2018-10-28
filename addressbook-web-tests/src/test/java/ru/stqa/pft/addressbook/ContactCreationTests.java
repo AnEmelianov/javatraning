47999403fd0d043bba5b5699bc69327be5dbfdbd
AnEmelianov <44379615+AnEmelianov@users.noreply.github.com>
2018-10-28 22:11:46 +0300

    homework4 lesson2 (upd)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -24,9 +24,9 @@ public class ContactCreationTests {
 
   private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
