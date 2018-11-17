33e2c30ea885ea1289c58ec73deea2bee6b6cb0f
AnEmelianov <44379615+AnEmelianov@users.noreply.github.com>
2018-11-18 01:05:57 +0300

    homework 13 lection6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,38 +1,79 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xStream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+    }.getType());
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+
+  //  @Test
+//  public void testContactCreation() {
+//    Contacts before = app.contact().all();
+//    File photo = new File("src/test/resources/test.png");
+//    ContactData contact = new ContactData().withFirstname("firstanme_new0000").withLastname("lastname_new").withLastname("lastname_new")
+//            .withHomePhone("111").withMobilePhone("222").withWorkPhone("333").withPhoto(photo);
+//    app.contact().create(contact, true);
+//    assertThat(app.contact().count(), equalTo(before.size() + 1));
+//    Contacts after = app.contact().all();
+//    assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+//  }
+
+
+  //  @Test(dataProvider = "validContactsFromJson")
+  @Test(dataProvider = "validContactsFromXml")
+  public void testContactCreation(ContactData contact) {
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/test.png");
-    ContactData contact = new ContactData().withFirstname("firstanme_new0000").withLastname("lastname_new").withLastname("lastname_new")
-            .withHomePhone("111").withMobilePhone("222").withWorkPhone("333").withPhoto(photo);
     app.contact().create(contact, true);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
-
-  @Test
-  public void testBadContactCreation() {
-    Contacts before = app.contact().all();
-    ContactData contact = new ContactData().withFirstname("firstanme_new0000;'")
-            .withLastname("lastname_new").withHomePhone("111").withMobilePhone("222").withWorkPhone("333");
-    app.contact().create(contact, true);
-    assertThat(app.contact().count(), equalTo(before.size()));
-    Contacts after = app.contact().all();
-    assertThat(after, equalTo(before));
-  }
-
 }
+
+
