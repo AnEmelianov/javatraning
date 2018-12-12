3c522d107207198f6fe20e88ae16734e10507831
AnEmelianov <44379615+AnEmelianov@users.noreply.github.com>
2018-12-13 00:37:47 +0300

    homework20 lection9

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/TestBase.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.testng.SkipException;
+
+import java.io.IOException;
+
+public class TestBase {
+
+   Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  public boolean isIssueOpen(int issueId) throws IOException {
+    String json = getExecutor().execute(Request.Get(String.format("http://bugify.stqa.ru/api/issues/%s.json", issueId))).returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    int state = parsed.getAsJsonObject().getAsJsonArray("issues").get(0).getAsJsonObject().get("state").getAsInt();
+    return (state == 0 || state == 1 || state == 4);
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+}
